0a57892e034d2fd3515aacceaa26260c6d4c98a6
gerald-gv <geraldgutierrez918@gmail.com>
2025-10-17 11:29:41 -0500

    fix: Se actualizo el client de supabase por exportacion directa al client / Se optimizaron los errores de las APIs

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -1,4 +1,4 @@
-import { client } from "@/supabase/client";
+import { supabase } from "@/supabase/client";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
@@ -6,18 +6,29 @@ export async function GET(request) {
     const headersList = await headers();
     const token = headersList.get("Josue");
 
-    if (!(token=== process.env.SI)){
-        return NextResponse.json({ error: "No autorizado"}, { status: 401 })
+    if (!(token === process.env.SI)) {
+        return NextResponse.json({ error: "No autorizado" }, { status: 401 })
     }
-    const supabase = await client();
-    if (!supabase.ok) {
-        console.error("Sucedio un error al jalar los datos")
-    }
-    const { data: category, error } = await supabase.from("category").select("*");
-    console.log(category);
     
+    //const supabase = await client();
+    // if (!supabase.ok) {
+    //     console.error("Sucedio un error al jalar los datos")
+    // }
+
+    const { data: category, error } = await supabase.from("category").select("*");
+
+    if (error) {
+        console.error("Error al jalar los datos", error.message)
+        return NextResponse.json(
+            { error: "Error interno del servidor" },
+            { status: 500 }
+        )
+    }
+
+    //console.log(category);
+
     return new Response(JSON.stringify(category), {
         status: 200,
-        headers: { "Content-Type": "application/json"}
+        headers: { "Content-Type": "application/json" }
     });
 }
--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,4 +1,4 @@
-import { client } from "@/supabase/client";
+import { supabase } from "@/supabase/client";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
@@ -9,12 +9,21 @@ export async function GET(request) {
     if (!(token=== process.env.SI)){
         return NextResponse.json({ error: "No autorizado"}, { status: 401 })
     }
-    const supabase = await client();
-    if (!supabase.ok) {
-        console.error("Sucedio un error al jalar los datos")
-    }
+    
+    //const supabase = await client();
+    // if (!supabase.ok) {
+    //     console.error("Sucedio un error al jalar los datos")
+    // }
+    
     const { data: products, error } = await supabase.from("product").select("*");
     
+    if(error){
+        console.error("Error al jalar los datos", error.message)
+        return NextResponse.json(
+            {error: "Error interno del servidor"},
+            {status: 500}
+        )
+    }
     return new Response(JSON.stringify(products), {
         status: 200,
         headers: { "Content-Type": "application/json"}
